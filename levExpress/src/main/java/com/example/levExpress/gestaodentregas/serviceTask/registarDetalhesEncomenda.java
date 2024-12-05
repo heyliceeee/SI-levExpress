ae96e26ffb2854005ed79561a7930fef20c294ab
heyliceeee <alicedias2002@hotmail.com>
2024-12-05 09:21:53 +0000

    ✨feat: obter detalhes da encomenda

--- levExpress/src/main/java/com/example/levExpress/gestaodentregas/serviceTask/registarDetalhesEncomenda.java
+++ levExpress/src/main/java/com/example/levExpress/gestaodentregas/serviceTask/registarDetalhesEncomenda.java
@@ -13,22 +13,33 @@ import java.util.Map;
 @Component
 public class registarDetalhesEncomenda {
 
-    List<Map<String, Object>> outputList = new ArrayList<>();
+    Map<String, Object> detalhesEncomenda;
     Gson gson = new Gson(); //converter as strings JSON em objetos
 
 
     @JobWorker(type = "registar-detalhes-encomenda", fetchAllVariables = true)
     public void handle(final JobClient client, final ActivatedJob job) {
         try {
-            System.out.println("criar pedido de entrega -> INPUT output: "+ outputList +"\n");
-
             Map<String, Object> variables = job.getVariablesAsMap();
 
-            //TODO: ver oq retorna aqui
-            System.out.println("VARIABLES: "+ variables);
-
             //TODO: resolver este erro
-            outputList = (List<Map<String, Object>>) variables.get("registar-detalhes-encomenda");
+            detalhesEncomenda = (Map<String, Object>) variables.get("registar-detalhes-encomenda");
+
+            // Exibir os detalhes no console
+            /*if (detalhesEncomenda != null) {
+                System.out.println("Origem: " + detalhesEncomenda.get("origem"));
+                System.out.println("Destino: " + detalhesEncomenda.get("destino"));
+                System.out.println("Peso: " + detalhesEncomenda.get("peso"));
+                System.out.println("Dimensões: " + detalhesEncomenda.get("dimensao"));
+                System.out.println("Data de Início: " + detalhesEncomenda.get("dataInicio"));
+                System.out.println("Duração: " + detalhesEncomenda.get("duracao"));
+                System.out.println("Foto: " + detalhesEncomenda.get("foto"));
+                System.out.println("Descrição: " + detalhesEncomenda.get("descricao"));
+            } else {
+                System.out.println("Variável 'registar-detalhes-encomenda' não encontrada!");
+            }*/
+
+            System.out.println("criar pedido de entrega -> INPUT output: "+ detalhesEncomenda +"\n");
         }
         catch (Exception e){
             System.err.println("Erro rastrear-encomenda -> " + e.getMessage());
