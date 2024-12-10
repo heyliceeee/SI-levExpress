da1840b647f53734404e64e7ddcf9a20fa84fd8b
ReginaPaz <125509895+Pazregina19@users.noreply.github.com>
2024-12-10 21:32:01 +0000

    Update and rename User.java to Pedido.java

---
+++ levExpress/src/main/java/com/example/levExpress/classes/Pedido.java
@@ -0,0 +1,41 @@
+package com.example.sistemaApoio.classes;
+
+import java.time.LocalDate;
+
+public class Pedido {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataResolucao;
+
+    public Pedido() {}
+
+    public Pedido(String titulo, String descricao, LocalDate dataResolucao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataResolucao = dataResolucao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataResolucao() {
+        return dataResolucao;
+    }
+
+    public void setDataResolucao(LocalDate dataResolucao) {
+        this.dataResolucao = dataResolucao;
+    }
+}
